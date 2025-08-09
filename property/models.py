be1d3154f584481a121aa3cfd1c9a36cd570893f
kamidzh <kamila.dzh09@gmail.com>
2025-08-09 12:53:56 +0300

    Высушила название поля

--- property/models.py
+++ property/models.py
@@ -67,8 +67,8 @@ class Complaint(models.Model):
 
 class Owner(models.Model):
     name = models.CharField('ФИО владельца', max_length=200, db_index=True)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20, db_index=True)
-    owner_pure_phone = PhoneNumberField('Нормализованный номер владельца', blank=True)
+    phonenumber = models.CharField('Номер владельца', max_length=20, db_index=True)
+    pure_phone = PhoneNumberField('Нормализованный номер владельца', blank=True)
     flats = models.ManyToManyField(
         Flat,
         related_name='owners',

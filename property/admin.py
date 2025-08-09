489a063151ecd9de2365beeb88e6b844169fcf66
kamidzh <kamila.dzh09@gmail.com>
2025-08-09 12:29:30 +0300

    Избавилась от дублирования данных в БД

--- property/admin.py
+++ property/admin.py
@@ -15,9 +15,9 @@ class OwnerInline(admin.TabularInline):
 
 @admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ['town', 'address', 'owner']
+    search_fields = ['town', 'address']
     readonly_fields = ['created_at']
-    list_display = ['address', 'price', 'new_building', 'construction_year', 'town', 'owners_phoneumber', 'owner_pure_phone']
+    list_display = ['address', 'price', 'new_building', 'construction_year', 'town']
     list_editable = ['new_building']
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
     raw_id_fields = ('liked_by', )
